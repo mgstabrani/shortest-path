f5ab564bd2c6306b02c8e65cf545fac68671573b
mgstabrani <mgstabrani19@gmail.com>
2021-04-05 20:59:18 +0700

    Edit interface

--- src/main.py
+++ src/main.py
@@ -1,53 +1,64 @@
 from astar import *
 from input import *
-import networkx as nx
-import matplotlib.pyplot as plt
+from mapVisualization import *
+from grafVisualization import *
 
 #Membuat graf dari file eksternal
 g = Graf(nodes, adjacentNode, nodeCoordinate)
 
-#Input node awal dan node akhir
-nodeFrom = input("Masukkan lokasi awal: ")
-nodeTo = input("Masukkan lokasi tujuan: ")
+while(True):
+    #Input node awal dan node akhir
+    print(50*"=")
+    nodeFrom = input("Masukkan lokasi awal: ")
+    nodeTo = input("Masukkan lokasi tujuan: ")
 
-#Mencari apakah nodeFrom dan nodeTo memiliki jalur
-hasil = []
-dikunjungi = []
-for i in range(g.getNumOfNode()):
-    dikunjungi.append(0)
-found = dfsExploreNode(g, nodeFrom, nodeTo, dikunjungi, hasil)
+    #Mencari apakah nodeFrom dan nodeTo memiliki jalur
+    hasil = []
+    dikunjungi = []
+    for i in range(g.getNumOfNode()):
+        dikunjungi.append(0)
+    found = dfsExploreNode(g, nodeFrom, nodeTo, dikunjungi, hasil)
 
-answer = []
-choosenEdges = []
+    answer = []
+    choosenEdges = []
 
-#Jika ada jalur
-if(found):
-    #Mencari jarak dan rute terpendek
-    answer = astar(g,nodeFrom, nodeTo)
-    print("Jarak terdekat: " + str(answer[1]) + " km")
-    print("Rute yang ditempuh: ", end=" ")
-    for i in range(len(answer[0])-1):
-        print(answer[0][i], end=" -> ")
-    print(answer[0][len(answer[0])-1])
+    print(50*"=")
+    #Jika ada jalur
+    if(found):
+        #Mencari jarak dan rute terpendek
+        answer = astar(g,nodeFrom, nodeTo)
+        print("Jarak terdekat: " + str(answer[1]) + " km")
+        print("Rute yang ditempuh: ", end=" ")
+        for i in range(len(answer[0])-1):
+            print(answer[0][i], end=" -> ")
+        print(answer[0][len(answer[0])-1])
 
-    #Set edge yang diwarnai berbeda
-    for i in range(len(answer[0]) - 1):
-        choosenEdges.append([answer[0][i],answer[0][i+1]])
+        #Set edge yang diwarnai berbeda
+        for i in range(len(answer[0]) - 1):
+            choosenEdges.append([answer[0][i],answer[0][i+1]])
 
-#Jika tidak ada jalur
-else:
-    print(nodeFrom, "ke", nodeTo, "tidak memiliki jalur.")
+    #Jika tidak ada jalur
+    else:
+        print(nodeFrom, "ke", nodeTo, "tidak memiliki jalur.")
 
-#Set posisi node
-pos = {}
-for i in range(g.getNumOfNode()):
-    pos[nodes[i]] = nodeCoordinate[i]
+    print(50*"=")
+    print("Visualisasi:")
+    print("1. Graf")
+    print("2. Map")
+    visualize = int(input("Masukkan pilihan: "))
 
-#Visulisasi Graph
-G = nx.Graph()
-G.add_nodes_from(nodes)
-G.add_edges_from(edges)
-nx.draw_networkx(G, pos, node_color="blue", font_size=8, edge_color="blue")
-if(found):
-    nx.draw_networkx(G, pos, nodelist=answer[0], node_color="red", font_size=8, edgelist=choosenEdges, edge_color="red")
-plt.show()
+    if(visualize == 1):
+        grafVisualization(g, nodes, edges, found, answer, choosenEdges)
+    elif(visualize == 2):
+        mapVisualization(g,nodeTo,nodes,answer, found, choosenEdges)
+
+    print(50*"=")
+    print("Lanjut?")
+    print("1. Ya")
+    print("2. Tidak")
+    pilihan = int(input("Masukkan pilihan: "))
+
+    if(pilihan == 1):
+        continue
+    elif(pilihan == 2):
+        exit()
