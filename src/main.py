c6debbe10af4b7d9328b38ce8c4ac53ec37af8d3
mgstabrani <mgstabrani19@gmail.com>
2021-04-05 14:37:57 +0700

    Resize font

--- src/main.py
+++ src/main.py
@@ -47,7 +47,7 @@ for i in range(g.getNumOfNode()):
 G = nx.Graph()
 G.add_nodes_from(nodes)
 G.add_edges_from(edges)
-nx.draw_networkx(G, pos, node_color="blue", edge_color="blue")
+nx.draw_networkx(G, pos, node_color="blue", font_size=8, edge_color="blue")
 if(found):
-    nx.draw_networkx(G, pos, nodelist=answer[0], node_color="red", edgelist=choosenEdges, edge_color="red")
+    nx.draw_networkx(G, pos, nodelist=answer[0], node_color="red", font_size=8, edgelist=choosenEdges, edge_color="red")
 plt.show()
